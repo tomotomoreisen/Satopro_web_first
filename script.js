dbb8b705619a4cb7eaed73bba03cae1dbde51a9b
john doe <tMacBook@tomoki-no-MacBook-Pro.local>
2019-05-19 20:26:29 +0900

    added dialog for intro, chancged desin

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
+$("#dialog_intro").dialog({ autoOpen: false, modal:true,  height: 750, width:750, draggable: false, show: "fade", hide: "fade", resizable: false ,open: function(){
+		$('.ui-widget-overlay').addClass('custom-overlay');
+	}
+});
 $("#intro").click(function(){
-	alert("intro clicked");
+	$("#dialog_intro").dialog("open");	
 })
 
 
