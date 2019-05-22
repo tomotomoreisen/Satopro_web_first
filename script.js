eef086e3470c522d6958f15831fe9174accf1fbb
john doe <tMacBook@tomoki-no-MacBook-Pro.local>
2019-05-22 11:35:57 +0900

    edited js , cs , html

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
-$("#dialog_intro").dialog({ autoOpen: false, modal:true,  height: 750, width:750, draggable: false, show: "fade", hide: "fade", resizable: false ,open: function(){
+$("#dialog_intro").dialog({ 
+	autoOpen: false, 
+	modal:true,  
+	height: 750, 
+	width:750, 
+	draggable: false, 
+	show: "fade", 
+	hide: "fade", 
+	resizable: false ,
+	open: function(){
 		$('.ui-widget-overlay').addClass('custom-overlay');
 	}
 });
 $("#intro").click(function(){
 	$("#dialog_intro").dialog("open");	
 })
-
+$("#dialog_intro .ui-dialog-titlebar-close").bind("click", function() {
+  return $("#dialog_intro").dialog("close");
+});
 
 })
