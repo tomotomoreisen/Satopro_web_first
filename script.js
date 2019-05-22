f0e6dfed25202dc1a345568fa226782a3c32384b
john doe <tMacBook@tomoki-no-MacBook-Pro.local>
2019-05-22 11:47:43 +0900

    edited script css etc

--- script.js
+++ script.js
@@ -19,4 +19,46 @@ $("#dialog_intro .ui-dialog-titlebar-close").bind("click", function() {
   return $("#dialog_intro").dialog("close");
 });
 
+$("#dialog_act").dialog({ 
+	autoOpen: false, 
+	modal:true,  
+	height: 750, 
+	width:750, 
+	draggable: false, 
+	show: "fade", 
+	hide: "fade", 
+	resizable: false ,
+	open: function(){
+		$('.ui-widget-overlay').addClass('custom-overlay');
+	}
+});
+$("#activity").click(function(){
+	$("#dialog_act").dialog("open");	
+})
+$("#dialog_act .ui-dialog-titlebar-close").bind("click", function() {
+  return $("#dialog_act").dialog("close");
+});
+
+$("#dialog_member").dialog({ 
+	autoOpen: false, 
+	modal:true,  
+	height: 750, 
+	width:750, 
+	draggable: false, 
+	show: "fade", 
+	hide: "fade", 
+	resizable: false ,
+	open: function(){
+		$('.ui-widget-overlay').addClass('custom-overlay');
+	}
+});
+$("#member").click(function(){
+	$("#dialog_member").dialog("open");	
+})
+$("#dialog_member .ui-dialog-titlebar-close").bind("click", function() {
+  return $("#dialog_member").dialog("close");
+});
+
+
+
 })
